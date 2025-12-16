cb073ae997d2db823a37429863d2889315f8b00e
saadan <sbengrich1@gmail.com>
2025-12-16 19:53:57 +0100

    Test: webhook Jenkins automatique

--- jenkins/POV-JAVA/src/main/java/com/example/Point/of/sale/PointOfSaleApplication.java
+++ jenkins/POV-JAVA/src/main/java/com/example/Point/of/sale/PointOfSaleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class PointOfSaleApplication {
+	// Test webhook Jenkins - 16/12/2025
 	public static void main(String[] args) {
 		SpringApplication.run(PointOfSaleApplication.class, args);
 	}
